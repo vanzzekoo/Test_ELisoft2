8ad6e8824c500e4205d29dfff93de8c3c7f44e65
vanzzekoo <138844903+vanzzekoo@users.noreply.github.com>
2023-07-07 20:26:22 +0700

    --
    --

--- database/migrations/2023_07_07_091452_create_users_table.php
+++ database/migrations/2023_07_07_091452_create_users_table.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -14,7 +17,16 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('status', [1, 2])->default(1);
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->timestamp('deleted_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,3 +41,13 @@ return new class extends Migration
         Schema::dropIfExists('users');
     }
 };
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+}
