dc18688ee3ea41446cef3a2f201f7c74a8bfec05
vanzzekoo <138844903+vanzzekoo@users.noreply.github.com>
2023-07-07 19:18:57 +0700

    --
    --

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,44 +2,70 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        // Kode untuk menampilkan daftar post
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
     }
 
     public function create()
     {
-        // Kode untuk menampilkan halaman form create post
+        return view('posts.create');
     }
 
     public function store(Request $request)
     {
-        // Kode untuk menyimpan data post baru
+        $validatedData = $request->validate([
+            'thumbnail' => 'required',
+            'title' => 'required',
+            'content' => 'required',
+            'status_publish' => 'required|in:draft,publish',
+            'tanggal_publikasi' => 'required|date',
+        ]);
+
+        Post::create($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
     }
 
     public function show($id)
     {
-        // Kode untuk menampilkan detail post berdasarkan ID
+        $post = Post::findOrFail($id);
+        return view('posts.show', compact('post'));
     }
 
     public function edit($id)
     {
-        // Kode untuk menampilkan halaman form edit post berdasarkan ID
+        $post = Post::findOrFail($id);
+        return view('posts.edit', compact('post'));
     }
 
     public function update(Request $request, $id)
     {
-        // Kode untuk mengupdate data post berdasarkan ID
+        $validatedData = $request->validate([
+            'thumbnail' => 'required',
+            'title' => 'required',
+            'content' => 'required',
+            'status_publish' => 'required|in:draft,publish',
+            'tanggal_publikasi' => 'required|date',
+        ]);
+
+        $post = Post::findOrFail($id);
+        $post->update($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
     }
 
     public function destroy($id)
     {
-        // Kode untuk menghapus post berdasarkan ID
-    }
+        $post = Post::findOrFail($id);
+        $post->delete();
 
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
+    }
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
@@ -34,6 +35,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace('App\Http\Controllers')
                 ->group(base_path('routes/web.php'));
         });
     }
