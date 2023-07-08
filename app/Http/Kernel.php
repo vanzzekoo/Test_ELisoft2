c59b7fb6e49673030960be6806c8252d649b7bea
vanzzekoo <138844903+vanzzekoo@users.noreply.github.com>
2023-07-09 00:13:13 +0700

    --
    --

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+         \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+         \Illuminate\Session\Middleware\AuthenticateSession::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,36 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+
+        return response()->json(['message' => 'Login successful']);
+    }
+
+    return response()->json(['message' => 'Invalid credentials'], 401);
+});
+
+
+// Logout
+Route::middleware('auth:sanctum')->post('/logout', function (Request $request) {
+    Auth::guard('web')->logout();
+
+    $request->session()->invalidate();
+    $request->session()->regenerateToken();
+
+    return response()->json(['message' => 'Logged out']);
 });
